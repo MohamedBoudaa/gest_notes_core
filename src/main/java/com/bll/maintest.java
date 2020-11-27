f2e2b57977b0a3ec3a306d301121069b954d19fc
MohamedBoudaa <60492162+MohamedBoudaa@users.noreply.github.com>
2020-11-27 20:00:18 +0100

    generic dao dependency
    Added generic dao dependency
    changed from old daogeneric to genericdao

--- src/main/java/com/bll/maintest.java
+++ src/main/java/com/bll/maintest.java
@@ -4,8 +4,8 @@ import com.bo.InscriptionAdministrative;
 import com.bo.InscriptionMatiere;
 import com.bo.InscriptionModule;
 import com.bo.Note;
-import com.dao.Dao;
-import com.dao.HibernateDaoImpl;
+import com.dao.HibernateGenericDao;
+import com.dao.IDao;
 
 public class maintest {
 
@@ -16,9 +16,9 @@ public class maintest {
 		InscriptionMatiere ins = new InscriptionMatiere();
 		Note n  = new Note();
 		
-		Dao<Long, InscriptionMatiere> dao = new HibernateDaoImpl<Long, InscriptionMatiere>(InscriptionMatiere.class);
+		IDao<Long, InscriptionMatiere> dao = new HibernateGenericDao<Long, InscriptionMatiere>(InscriptionMatiere.class);
 		
 		
-		dao.create(ins);
+		dao.save(ins);
 	}
 }
