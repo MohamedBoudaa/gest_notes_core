b8f02b0577ccfb4d560ee0734ec6ce63a924b9d1
MohamedBoudaa <60492162+MohamedBoudaa@users.noreply.github.com>
2020-12-02 23:15:31 +0100

    InscriptionManager
    Implementing ManagerFactory system in order to achieve dependency injection pattern

--- src/main/java/com/bll/InscriptionManagerImpl.java
+++ src/main/java/com/bll/InscriptionManagerImpl.java
@@ -1,13 +1,43 @@
 package com.bll;
 
+import com.bo.Etudiant;
+import com.bo.Niveau;
 import com.dao.DaoFactory;
 import com.dao.InscAdminDao;
+import com.dao.InscMatiereDao;
+import com.dao.InscModuleDao;
+import com.dao.InscPedagoDao;
+import com.dao.impl.InscMatiereDaoImpl;
 
 public class InscriptionManagerImpl implements InscriptionManager{
 	
 	
 	private InscAdminDao inscAdminDao = (InscAdminDao) DaoFactory.getDaoFactory().getDao(DaoFactory.DAO_INSCADMIN);
 	
-	
+	private InscMatiereDao inscMatiereDao = (InscMatiereDao) DaoFactory.getDaoFactory().getDao(DaoFactory.DAO_INSCMATIERE);
 
+	private InscPedagoDao inscPedagoDao = (InscPedagoDao) DaoFactory.getDaoFactory().getDao(DaoFactory.DAO_INSCPEDAGO);
+
+	private InscModuleDao inscModuleDao = (InscModuleDao) DaoFactory.getDaoFactory().getDao(DaoFactory.DAO_INSCMODULE);
+
+	
+	
+	/**
+	 * Constructor
+	 */
+	public InscriptionManagerImpl() {
+		
+	}
+	
+	public void InscrirAdmin(Etudiant etudiant,Niveau niveau,int year) {
+		
+		if(inscAdminDao.exists(etudiant,year)) {
+			
+		}
+		
+	}
+	
+	
+	
+	
 }
