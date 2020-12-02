012be138cf69137fe13483b0fe758aee5dc120e8
youss999 <63669195+youss999@users.noreply.github.com>
2020-12-02 18:45:34 +0100

    Implementation
    Impl de la fonction exists pour inscAdmin

--- src/main/java/com/dao/impl/InscAdminDaoImpl.java
+++ src/main/java/com/dao/impl/InscAdminDaoImpl.java
@@ -1,7 +1,18 @@
 package com.dao.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 import com.bo.Etudiant;
 import com.bo.InscriptionAdministrative;
+import com.dao.DaoException;
+import com.dao.DaoFactory;
+import com.dao.EtudiantDao;
 import com.dao.HibernateGenericDao;
 import com.dao.InscAdminDao;
 
@@ -13,9 +24,36 @@ public class InscAdminDaoImpl extends HibernateGenericDao<Long, InscriptionAdmin
 	}
 
 	@Override
-	public boolean exists(Etudiant e, int year) {
-		// TODO Auto-generated method stub
+	public boolean exists(Etudiant e, int y) {
+		
+		Session s = this.getSf().getCurrentSession();
+		Transaction tx = null;
+		List<InscriptionAdministrative> list = new ArrayList();
+		try {
+			tx = s.beginTransaction();
+
+			Query query = s.createQuery("from InscriptionAdministrative where idEtudiant=:id and year=:y");
+			query.setParameter("id", e.getId());
+			query.setParameter("y", y);
+					
+			list=query.getResultList();
+			
+			tx.commit();
+		} catch (HibernateException ex) {
+			LOGGER.debug("error due to :" + ex);	
+			if (tx != null) {
+				tx.rollback();
+			}
+			throw new DaoException(ex);
+		} finally {
+			if (s != null && s.isOpen()) {
+				s.close();
+			}
+		}
+		if(list.size() !=0 ) {
+				return true;
+		}
 		return false;
 	}
-
+	
 }
